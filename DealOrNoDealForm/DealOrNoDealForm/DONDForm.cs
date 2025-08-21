ebdabcb4bae8e0e3705f4fd22e5c630eb118d174
Nairn White <whitenairn8@gmail.com>
2025-08-21 17:39:47 +0100

    created initial form

---
+++ DealOrNoDealForm/DealOrNoDealForm/DONDForm.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DealOrNoDealForm
+{
+    public partial class DONDForm : Form
+    {
+        public DONDForm()
+        {
+            InitializeComponent();
+        } 
+    }
+}
